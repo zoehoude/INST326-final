74119ec8b46d84c1c0cc76b3373cf0b1f45ae9dd
EthanMeyer41 <102557683+EthanMeyer41@users.noreply.github.com>
2022-04-15 21:01:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+"""Zoe Houde & Ethan Meyer
+Final Project INST326"""
+
+"""Our Python File will be seperated by three parts: classes, functions, and functionality."""
+
+#classes
+
+class Fraternity:
+    fraternity = []
+    def __init__(self, name, location, time, meals):
+        self.name = name
+        self.location = location
+        self.time = time
+        self.meals = meals 
+        
+class Sorority:
+    sororities = []
+
+    def __init__(self, name, location, time, meals):
+        self.name = name
+        self.location = location
+        self.time = time
+        self.meals = meals 
+    
+    
+
+class Service_Center:
+    service_cetner = []
+    """This empty array will contain the name of serice_centers on file as well as their location"""
+    pass
+
+
+
+#Functions. These functions will exist within both sorority and fratnerity classes
+def check_food():
+    """This function will confirm the donation information from the upload_donation function"""
+    pass
+    
+    
+def upload_donation(donationn):
+    """either sorority or fraternity object will upload their name, type of food, time for pick up, and location """
+    pass
+
+
+
+
+
+#functionality
+def main(daily):
+    pass
+
+if __name__ == "__main__":
+    main(daily)
