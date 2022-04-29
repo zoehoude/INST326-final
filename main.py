3759a26397ac5b9aaae6206fc266b5d52dcd4128
EthanMeyer41 <102557683+EthanMeyer41@users.noreply.github.com>
2022-04-29 19:50:49 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ Final Project INST326"""
 
 """This is the hardoced version of the outline. We need to get the array saved so its not hardcoded in by using self. methods
 I was was also going to add a more complex imput for meal, amount just stood as a place holder"""
+import re
 class Fraternity:
     def __init__(self, frat):
         self.frat = frat
         entree = input("Entree?")
         sides = input("sides?")
         packaged = input("pack?")
-        order = [entree, sides, packaged]
+        servings = input("How many servings do you have leftover?:")
+        allergens = input("Does this food have potential allergens?:")
+        order = [entree, sides, packaged, servings, allergens]
         finished = [frat, order]
         print(finished)
 
@@ -17,42 +20,53 @@ class Sorority:
     def __init__(self, sor):
         self.sor = sor
         entree = input("Entree?")
-        sides = input("sides?")
+        sides = input("sides?") 
         packaged = input("pack?")
-        order = [entree, sides, packaged]
+        servings = input("How many servings do you have leftover?:")
+        allergens = input("Does this food have potential allergens?:")
+        order = [entree, sides, packaged, servings, allergens]
         finished = [sor, order]
         print(finished)
        
 
-#acting as main. should be a loop for each time a new input is created, a new frat or sorioert
+#acting as main. should be a loop for each time a new input is created, a new frat or soriority
+#regex functions limit user input to avoid errors in address, food input, name input, etc.
+
+frats_dict = {}
+sor_dict = {}
 print("welcome")
-x = input("Frat or Sor?:")
-if x == "Frat":
+x = input("To begin, please state if you represent either a Fraternity or Sorority?:")
+if x =="Fraternity":
+    print("To begin, please enter your greek letters, if you are not in our system, we will add you:")
     Fname = input("Greek Letters: ")
-    Flocation = input("Street Address as '123 Rocky Road': ")
-    frat = [Fname, Flocation]
-    Fraternity(frat)
-    exit()
+    if Fname not in frats_dict:
+        print("It appears you are not in our database, please enter your address like '123 Rocky Road'")
+        Faddy = input()
+        frats_dict.update({Fname:Faddy})
+        Fraternity(Fname)
+        exit()       
     
-if x == "Sor":
+elif x =="Sorority":
+    print("To begin, please enter your greek letters, if you are not in our system, we will add you:")
     Sname = input("Greek Letters: ")
-    Slocation = input("Street Address as '123 Rocky Road': ")
-    sor = [Sname, Slocation]
-    Sorority(sor)
-    exit()
-    
-    
-
-
-
-
-
-def add_order(frats): 
-    choice = input("Submit an order?(yes or no)")
-    if choice =="no":
-        x = print("Ok, returning you back to home")
-        return x
+    if Sname not in sor_dict:
+        print("It appears you are not in our database, please enter your address like '123 Rocky Road'")
+        Saddy = input()
+        frats_dict.update({Sname:Saddy})
+        Sorority(Sname)
+        exit()
         
+  
+else:
+    print("please try again")
+
+
+
+
+
+
+
     
+
 
 
